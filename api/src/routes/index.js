bd303417e6a4d8bb30c730514b4f1fbe770d7623
Analiadlt <87041305+Analiadlt@users.noreply.github.com>
2021-11-21 21:30:21 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,6 +7,18 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const fetch = require ('node-fetch');
 
+router.get ('/pokemons', async (req, res)=>{
+	const api_url='http://pokeapi.co/api/v2/pokemon';
+	const fetch_response = await fetch(api_url);
+	const json = await fetch_response.json();
+	//habría que hacer una subrequest para obtener los datos necesarios
+	//pokemon_name = ¿¿¿json.name???
+	//api_url2='http://pokeapi.co/api/v2/pokemon/${pokemon_name}'
+	//debería devolver sólo: imagen - nombre - tipos (electrico, fuego, agua, etc)
+	console.log(json);
+	resp.json(json);
+});
 
 module.exports = router;
