609f5c4a1fa29e2527e3ecaa9bab361ee5d961fc
unknown <delatorreanalia@google.com>
2023-05-23 11:38:09 -0300

    modify enviroment DB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
+const port = process.env.PORT || 3001;
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
