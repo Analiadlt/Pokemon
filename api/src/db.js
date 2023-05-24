6b151508caefef88c704c8a2de1215d7e8df90ae
unknown <delatorreanalia@google.com>
2023-05-24 11:04:44 -0300

    modify db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-// const db = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`;
-const { DB_CONFIG_LINK } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const db = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`;
 
-const sequelize = new Sequelize(DB_CONFIG_LINK, {
+const sequelize = new Sequelize(db, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
